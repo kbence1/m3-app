4d5891b4ef2acd457bd817682d9b7ec41ae66ca9
kissbence222@gmail.com <kissbence222@gmail.com>
2023-12-19 20:34:19 +0000

    Created Main, Adatlap and RiasztasiAdatok; Edited settings; Deleted Form1

---
+++ client_code/Main/__init__.py
@@ -0,0 +1,65 @@
+from ._anvil_designer import MainTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
+from ..RiasztasiAdatok import RiasztasiAdatok
+
+class Main(MainTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    anvil.users.login_with_form()
+
+    # set each Link's `tag.form_to_open` attribute to an instance of the Form you want to open
+    self.riasztasi_adatok.tag.form_to_open = RiasztasiAdatok()
+    
+
+    # Any code you write here will run before the form opens.
+
+  def riasztási_adatok_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    """A generalised click handler that you can bind to any nav link."""
+    # Find out which Form this Link wants to open
+    form_to_open = event_args['sender'].tag.form_to_open
+
+    self.content_panel.clear()
+    self.content_panel.add_component(form_to_open)
+
+  def resztvevok_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    """A generalised click handler that you can bind to any nav link."""
+    # Find out which Form this Link wants to open
+    form_to_open = event_args['sender'].tag.form_to_open
+
+    self.content_panel.clear()
+    self.content_panel.add_component(form_to_open)
+
+  def riasztott_szerek_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    """A generalised click handler that you can bind to any nav link."""
+    # Find out which Form this Link wants to open
+    form_to_open = event_args['sender'].tag.form_to_open
+
+    self.content_panel.clear()
+    self.content_panel.add_component(form_to_open)
+
+  def riasztás_lezarasa_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    """A generalised click handler that you can bind to any nav link."""
+    # Find out which Form this Link wants to open
+    form_to_open = event_args['sender'].tag.form_to_open
+
+    self.content_panel.clear()
+    self.content_panel.add_component(form_to_open)
+
+  def dokumentumok_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    """A generalised click handler that you can bind to any nav link."""
+    # Find out which Form this Link wants to open
+    form_to_open = event_args['sender'].tag.form_to_open
+
+    self.content_panel.clear()
+    self.content_panel.add_component(form_to_open)
--- client_code/RiasztasiAdatok/__init__.py
+++ client_code/RiasztasiAdatok/__init__.py
@@ -1,15 +1,20 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import RiasztasiAdatokTemplate
 from anvil import *
 import anvil.server
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
 
-class Form1(Form1Template):
+class RiasztasiAdatok(RiasztasiAdatokTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    anvil.users.login_with_form()
+
+    riasztast_ado_list = []
+    for row in app_tables.riasztastado.search():
+        riasztast_ado_list.append((row["ShortName"], row))
+    
+    self.riasztast_igenylo.items = riasztast_ado_list
 
     # Any code you write here will run before the form opens.

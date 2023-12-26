4a7141add599e58bbf971d0cd3c53a90de52b7ae
kissbence222@gmail.com <kissbence222@gmail.com>
2023-12-26 22:19:45 +0000

    Edited Main and ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -18,4 +18,4 @@ import anvil.server
 #
 @anvil.server.callable
 def create_user():
-  anvil.users.signup_with_email("kissbence222@gmail.com","asd123");
+  anvil.users.signup_with_email('kissbence222@gmail.com','tjaFy3qZS4BdUeJfEcLWnz');
